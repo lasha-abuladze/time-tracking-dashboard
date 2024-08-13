8a9a528ae93bcea69bc4c1107d7516a9a940e432
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2024-08-14 03:12:44 +0400

    time tracking dashboard

--- vsFiles/script.js
+++ vsFiles/script.js
@@ -1,18 +1,69 @@
 `use strict`;
 
-const main = document.querySelector(`#main`)
-
+const main = document.querySelector(`#main`);
 const reportEx = document.querySelector(`.report`);
+
+const daily = document.querySelector(`#daily`);
+const weekly = document.querySelector(`#weekly`);
+const monthly = document.querySelector(`#monthly`);
+const inpArray = Array.from(document.querySelectorAll(`.inp`));
+const lbArray = Array.from(document.querySelectorAll(`.lb`));
+
+lbArray[1].classList.add(`color-white`)
+
+
+// inpArray.forEach((el,i) => {
+//     if(el.checked) {
+//         lbArray[i].classList.add(`white-color`);
+//     }
+// })
 
 reportEx.classList.add(`display-none`);
 
-
-
 fetch(`./data.json`).then(res => res.json()).then(data => {
 
-    displayReports(data)
+    displayReports(data);
+
+    const currentHrs = Array.from(document.querySelectorAll(`.current-hours`));
+    const previousHrs = Array.from(document.querySelectorAll(`.previous-hours`));
+
+
+    inpArray.forEach(el => {
+        el.addEventListener(`click`, () => {      
+            
+            inpArray.forEach((el,i) => {
+                if(el.checked) {
+                    lbArray[i].classList.add(`color-white`);
+                } else {
+                    lbArray[i].classList.remove(`color-white`);
+                }
+            })
+            
+    
+            if(daily.checked) {
+                data.forEach((el, i) => {
+                    currentHrs[i].textContent = `${data[i].timeframes.daily.current}hrs`
+                    previousHrs[i].textContent = `Last Week - ${data[i].timeframes.daily.previous}hrs`
+                
+                })                
+            } else if (monthly.checked) {
+                data.forEach((el, i) => {
+                    currentHrs[i].textContent = `${data[i].timeframes.monthly.current}hrs`
+                     previousHrs[i].textContent = `Last Week - ${data[i].timeframes.monthly.previous}hrs`
+                })
+            } else {
+                data.forEach((el, i) => {
+                    currentHrs[i].textContent = `${data[i].timeframes.weekly.current}hrs`
+                     previousHrs[i].textContent = `Last Week - ${data[i].timeframes.weekly.previous}hrs`
+                })
+            }
+        })
+    })
+
+
 
 });
+
 
 
 
@@ -37,8 +88,8 @@ const displayReports =function (data) {
                 </div>
 
                 <div class="hours">
-                    <strong>${el.timeframes.weekly.current}hrs</strong>
-                    <span>Last Week - ${el.timeframes.weekly.previous}hrs</span>
+                    <strong class="current-hours">${el.timeframes.weekly.current}hrs</strong>
+                    <span class="previous-hours" >Last Week - ${el.timeframes.weekly.previous}hrs</span>
                 </div>
 
             </div>
